60f8d744b8cba1c86c65f0c19db2626998fc4bfc
Arseniy Savin <ayusavin@fantast.dev>
2023-07-18 15:03:20 +0000

    Added Cloudflare Pages deployment action
    Added deployment config, Makefile for project building and commented some unusing things (linter swears).

--- src/features/createRoom/ui/CreateRoomForm.tsx
+++ src/features/createRoom/ui/CreateRoomForm.tsx
@@ -59,29 +59,29 @@ const Text = styled.p`
     width: 203px;
 `
 
-const CreateButton = styled.button`
-    font-family: var(--font);
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    color: var(--orange);
+// const CreateButton = styled.button`
+//     font-family: var(--font);
+//     font-size: 18px;
+//     font-style: normal;
+//     font-weight: 400;
+//     line-height: normal;
+//     color: var(--orange);
 
-    display: flex;
-    height: 44px;
-    justify-content: center;
-    align-items: center;
-    padding: 0 10px;
-    border-radius: 8px;
-    border: 1px solid var(--orange);
-    background: var(--white);
-    box-shadow: 0px 2px 6px 0px #C5CCD5;
-    transition: all .3s ease;
-    &:hover {
-        color: var(--white);
-        background: var(--orange);
-    }
-`
+//     display: flex;
+//     height: 44px;
+//     justify-content: center;
+//     align-items: center;
+//     padding: 0 10px;
+//     border-radius: 8px;
+//     border: 1px solid var(--orange);
+//     background: var(--white);
+//     box-shadow: 0px 2px 6px 0px #C5CCD5;
+//     transition: all .3s ease;
+//     &:hover {
+//         color: var(--white);
+//         background: var(--orange);
+//     }
+// `
 
 export const CreateRoomForm: React.FC = () => {
   return (
