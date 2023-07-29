f105db131c0ed354cb3dd780bc999b5439e3ca48
unknown <emilnovikov392@gmail.com>
2023-07-30 01:59:20 +0600

    layout widget call controllers

--- src/pages/CallPage/CallPage.tsx
+++ src/pages/CallPage/CallPage.tsx
@@ -1,44 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Call } from '../../features/Call';
-import ControllersWrapper from '../../widgets/layout/ControllersWrapper';
-import { MicrophoneController } from '../../features/MicrophoneController';
-import { CameraController } from '../../features/CameraController';
-import { RecordController } from '../../features/RecordController';
-import { HandController } from '../../features/HandController/ui/HandController';
-import { ScreenController } from '../../features/ScreenController';
-import { MenuController } from '../../features/ControllersMenu';
-import { LeaveCall } from '../../features/LeaveCall';
-
-const Left = styled.div`
-  display: flex;
-  gap: 20px;
-  justify-content: space-between;
-`
-
-const Center = styled.div`
-  display: flex;
-  gap: 20px;
-  justify-content: space-between;
-`
+import CallControllers from '../../widgets/CallControllers';
 
 const CallPage: React.FC = () => {
   return (
     <div>
       <Call/>
-      <ControllersWrapper>
-        <Left>
-          <MicrophoneController/>
-          <CameraController/>
-        </Left>
-        <Center>
-          <HandController/>
-          <ScreenController/>
-          <RecordController/>
-          <MenuController/>
-        </Center>
-        <LeaveCall/>
-      </ControllersWrapper>
+      <CallControllers/>
     </div>
   )
 }
