eed9771f97988c53c4f631ab96c2448c8e02930b
unknown <emilnovikov392@gmail.com>
2023-08-02 03:54:24 +0600

    layout lesson info

--- src/widgets/CallInfo.tsx
+++ src/widgets/CallInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { CopyLink } from '../features/Copy';
 
 const Container = styled.div`
     display: flex;
@@ -8,13 +9,41 @@ const Container = styled.div`
     justify-content: space-between;
     align-items: center;
     margin-top: 16px;
-    border: 1px #000 solid;
+`
+
+const Info = styled.div`
+  display: grid;
+  width: 295px;
+  grid-template-columns: 255px 18px;
+  gap: 0 14px;
+  align-items: center;
+`
+
+const Title = styled.h1`
+  font-family: var(--font);
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+const Paragraph = styled.p`
+  font-family: var(--font);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
 `
 
 const CallInfo: React.FC = () => {
   return (
     <Container>
-        Call Info
+      <Info>
+        <Title>Информатика 4 класс</Title>
+        <CopyLink/>
+        <Paragraph>18 участников|01:12:45 </Paragraph>
+      </Info>
+
     </Container>
   )
 }
